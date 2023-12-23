8fe440b6ca27163c5e74e2cb1d2b89c367a909fb
Mayank Solanki <72220404+mayank-ji@users.noreply.github.com>
2023-12-23 21:30:54 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style/main.scss' ;
-import './main.css' ;
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
